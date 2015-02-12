b11f24734f9f4a3edc34adc3889ebb2d3fa0743b
Jose Alexis Correa valencia <jalexiscv@gmail.com>
2015-02-12 11:33:37 -0500

    Update Push.class.php

--- Push.class.php
+++ Push.class.php
@@ -16,7 +16,6 @@
  */ 
 
 class Push{ 
-     
 
     public $androidAuthKey = "Android Auth Key Here"; 
     public $iosApnsCert = "./certification/xxxxx.pem"; 
@@ -55,33 +54,24 @@ class Push{
         //result sample {"multicast_id":6375780939476727795,"success":1,"failure":0,"canonical_ids":0,"results":[{"message_id":"0:1390531659626943%6cd617fcf9fd7ecd"}]} 
         //http://developer.android.com/google/gcm/http.html // refer error code 
         curl_close($ch); 
-        
         $rtn["code"] = "000";//means result OK 
         $rtn["msg"] = "OK"; 
         $rtn["result"] = $result; 
-        return $rtn; 
-        
+        return($rtn); 
      } 
-     
-     
     /** 
      * For IOS APNS 
      * $params["msg"] : Expected Message For APNS 
      */ 
     private function sendMessageIos($registration_id, $params) { 
-        
         $ssl_url = 'ssl://gateway.push.apple.com:2195'; 
         //$ssl_url = 'ssl://gateway.sandbox.push.apple.com:2195; //For Test 
-        
         $payload = array(); 
         $payload['aps'] = array('alert' => array("body"=>$params["msg"], "action-loc-key"=>"View"), 'badge' => 0, 'sound' => 'default'); 
         ## notice : alert, badge, sound 
-
-                 
         ## $payload['extra_info'] is different from what your app is programmed, this extra_info transfer to your IOS App 
         $payload['extra_info'] = array('apns_msg' => $params["msg"]); 
         $push = json_encode($payload); 
-         
         //Create stream context for Push Sever. 
         $streamContext = stream_context_create(); 
         stream_context_set_option($streamContext, 'ssl', 'local_cert', $this->iosApnsCert); 
@@ -93,29 +83,21 @@ class Push{
             $rtn["msg"] = "Failed to connect ".$error." ".$errorString; 
             return $rtn; 
         } 
-         
         //echo 'error=' . $error; 
         $t_registration_id = str_replace('%20', '', $registration_id); 
         $t_registration_id = str_replace(' ', '', $t_registration_id); 
         $apnsMessage = chr(0) . chr(0) . chr(32) . pack('H*', str_replace(' ', '', $t_registration_id)) . chr(0) . chr(strlen($push)) . $push; 
-         
         $writeResult = fwrite($apns, $apnsMessage); 
         fclose($apns); 
-        
         $rtn["code"] = "000";//means result OK 
         $rtn["msg"] = "OK"; 
         return $rtn; 
-
     }//private function sendMessageIos($registration_id, $msg, $link, $type) { 
-           
-            
-        
        /** 
      * Send message to SmartPhone 
      * $params [pushtype, msg, registration_id] 
      */ 
        public function sendMessage($params){ 
-           
         //$parm = array("msg"=>$params["msg"]); 
         if($params["registration_id"] && $params["msg"]){ 
                switch($params["pushtype"]){ 
@@ -141,14 +123,11 @@ class Push{
     public function registration($params){ 
         $pushtype = $params["pushtype"]; 
         $idphone = $params["idphone"]; 
-        
         print_r($params); 
         //{insert into database} 
         echo json_encode($rtn); 
     } 
-     
-     
-    /** 
+     /** 
      * Step 2. 
      * Send message to each iphone from web App. 
      * @params : Array() : messages () 
@@ -169,12 +148,5 @@ class Push{
                 }//switch($val["pushtype"]){ 
             }//foreach($rows as $key => $val){ 
         }//if(is_array($rows)){ 
-
-         
-    }//function send(){ 
-            
-         
-     
-
-                 
+    }
 } 
